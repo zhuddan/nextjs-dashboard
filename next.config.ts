208669765b42e921c074b8c0c806fc55f69a7a77
zhuddan <yobaci@163.com>
2024-11-03 19:07:34 +0800

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   // strictMode: true,
+  // experimental: {
+  //   ppr: 'incremental',
+  // },
 };
 
 export default nextConfig;
