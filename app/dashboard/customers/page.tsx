0820a37cf86ed00bc82b1abf03272f0ebc30b3a3
zhuddan <yobaci@163.com>
2024-11-04 16:21:26 +0800

    优化客户页面，添加客户数据获取和筛选功能

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-  return 'customers'
+import { fetchCustomers, fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams
+  const query = searchParams?.query ?? ''
+  console.log(query)
+  const customers = await fetchFilteredCustomers(query)
+  return <CustomersTable customers={customers} />
 }
